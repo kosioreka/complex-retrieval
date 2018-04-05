50254f811be26fc13843c80c08a98175b2b1a286
Anna <annakosiorek4@gmail.com>
2018-04-05 20:26:47 +0200

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='trec-car-tools',
+    version='2.0',
+    packages=['trec_car'],
+    url='trec-car.cs.unh.edu',
+    license='BSD 3-Clause',
+    author='laura-dietz',
+    author_email='Laura.Dietz@unh.edu',
+    description='Support tools for TREC CAR participants',
+    install_requires=['cbor>=0.1.4', 'typing'],
+)
