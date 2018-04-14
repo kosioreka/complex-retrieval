fc3a8dfe6d467a0a113f00dc5b312c0ef3e4a09f
Bartosz Z <motosx1@gmail.com>
2018-04-15 01:22:55 +0200

    word2vec rankings

--- read_folds.py
+++ read_folds.py
@@ -4,7 +4,7 @@ from text_preprocess import Preprocessing
 
 class FoldsTraining(object):
 
-    def __init__(self, file_name='', directory_name=''):
+    def __init__(self, file_name='', directory_name='', nb_folds=2):
         if directory_name is not '':
             self.directory_name = directory_name + '/'
         else:
@@ -19,21 +19,25 @@ class FoldsTraining(object):
         self.true_relevance = []
         self.true_relevance_stemmed = []
         self.queries = []
+        self.queries_all = []
         self.paragraphs = []
+        self.paragraphs_dict = {}
         self.corpus = []
-        self.read_folds()
+        self.read_folds(nb_folds)
 
-    def read_folds(self):
-        folds_amount = range(1)  # 5
+    def read_folds(self, nb_folds=2):
+        folds_amount = range(nb_folds)  # 5
 
         for i in folds_amount:
             file_reader = Preprocessing(self.outlines_file.format(nr=i), self.paragraphs_file.format(nr=i))
             with open(self.qrels_file.format(nr=i), 'r') as f:
                 qrels = load_qrels(f)
                 self.true_relevance.append(qrels)
-                self.true_relevance_stemmed = file_reader.preprocess_stem_qrels(qrels)
-            self.queries.append(file_reader.get_raw_queries())
+                self.true_relevance_stemmed.append(file_reader.preprocess_stem_qrels(qrels))
+            self.queries.append(file_reader.get_raw_queries(qe_synonyms=False))
+            self.queries_all.extend(file_reader.get_raw_queries())
             self.paragraphs.append(file_reader.get_raw_paragraphs())
+            self.paragraphs_dict.update(file_reader.get_raw_paragraphs())
             self.corpus.extend([v for k, v in self.paragraphs[i].items()])
 
     # def compute(self):
